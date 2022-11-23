c7e6bde73a6b0e51a6315e8332aedb0702e201b8
sonsonghyun <98512998+sonsonghyun@users.noreply.github.com>
2022-11-24 08:21:58 +0900

    221124
    221124

--- example/src/b_2/java11557.java
+++ example/src/b_2/java11557.java
@@ -1,7 +1,8 @@
-import java.util.*;
+package b_2;
 
-public class Main {
+import java.util.Scanner;
 
+public class java11557 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
@@ -27,25 +28,3 @@ public class Main {
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
