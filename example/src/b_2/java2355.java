67d8d774a213ccc6a6689e0b59be0375697181f9
sonsonghyun <98512998+sonsonghyun@users.noreply.github.com>
2022-11-25 20:23:04 +0900

    221125
    221125

--- example/src/b_2/java2355.java
+++ example/src/b_2/java2355.java
@@ -1,8 +1,11 @@
-import java.io.*;
-import java.util.*;
+package b_2;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class java2355 {
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(in.readLine());
@@ -17,26 +20,4 @@ public class Main {
         }
         System.out.println(sum);
     }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
