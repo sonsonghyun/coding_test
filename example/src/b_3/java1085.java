944552b4395a07795d7f8a8221197b3a3be8e466
sonsonghyun <98512998+sonsonghyun@users.noreply.github.com>
2022-12-08 19:36:07 +0900

    221208
    221208

---
+++ example/src/b_3/java1085.java
@@ -0,0 +1,19 @@
+package b_3;
+import java.util.*;
+
+public class java1085 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int x = in.nextInt();
+        int y = in.nextInt();
+        int w = in.nextInt();
+        int h = in.nextInt();
+
+        int x_min = Math.min(x, w-x);
+        int y_min = Math.min(y, h-y);
+
+
+        System.out.println(Math.min(x_min, y_min));
+    }
+}
