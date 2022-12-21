59f974882f2ee43dac2858cef47cb8b806db2248
sonsonghyun <98512998+sonsonghyun@users.noreply.github.com>
2022-12-21 17:45:39 +0900

    221221
    221221

---
+++ example/src/b_2/java11944.java
@@ -0,0 +1,33 @@
+package b_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class java11944 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br;
+        br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String n = st.nextToken();
+        int numberN = Integer.valueOf(n);
+        int m = Integer.valueOf(st.nextToken());
+        int len = n.length();
+
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < numberN * len && i <= m; i += len) {
+            sb.append(n);
+        }
+
+        if(sb.length() > m) {
+            System.out.print(sb.toString().substring(0, m));
+        }
+        else {
+            System.out.print(sb.toString());
+        }
+    }
+}
