db4ab7a5fdbae68e5c0c729dcffbcc0d5193b2f9
sonsonghyun <98512998+sonsonghyun@users.noreply.github.com>
2022-12-28 21:04:03 +0900

    221228
    221228

---
+++ example/src/b_1/java2163.java
@@ -0,0 +1,19 @@
+package b_1;
+
+import java.util.Scanner;
+
+public class java2163 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int count = 0;
+
+        count = m * n - 1;
+        System.out.println(count);
+
+    }
+
+}
