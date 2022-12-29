e990e981e26a8debadf02cc459c2150446453c74
sonsonghyun <98512998+sonsonghyun@users.noreply.github.com>
2022-12-29 19:28:22 +0900

    221229
    221229

---
+++ example/src/b_1/java10769.java
@@ -0,0 +1,29 @@
+package b_1;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class java10769 {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        int smile = (str.length() - str.replace(":-)", "").length()) / 3;
+        int sad = (str.length() - str.replace(":-(", "").length()) / 3;
+
+        String ret = "";
+        if (smile == 0 && sad == 0) {
+            ret = "none";
+        } else if (smile > sad) {
+            ret = "happy";
+        } else if (sad > smile) {
+            ret = "sad";
+        } else {
+            ret = "unsure";
+        }
+        System.out.println(ret);
+        sc.close();
+    }
+}
