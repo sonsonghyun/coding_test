590cf71264bc45558428f9a2462bdd58133fc3a7
sonsonghyun <98512998+sonsonghyun@users.noreply.github.com>
2023-01-02 21:00:18 +0900

    230102
    230102

---
+++ example/src/b_1/java1145.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class java1145 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int val = 1;
+        int count = 0;
+
+        while (true) {
+            for (int k = 0; k < 5; k++) {
+                if (val % arr[k] == 0) {
+                    count++;
+                }
+            }
+            if (count >= 3) {
+                System.out.println(val);
+                return;
+            }
+            count = 0;
+            val++;
+        }
+
+    }
+}
